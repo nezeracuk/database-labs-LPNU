8437a070001b929ba4fe31d13cc5f596b788d860
nezeracuk <skibickignazar@gmail.com>
2025-10-02 19:38:07 +0300

    Fix app.py imports and make Flask listen on all interfaces

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-from app import create_app, execute_sql_scripts, populate_data, create_tables, create_database
+from app import create_app
 
 app = create_app()
 
-with app.app_context():
-    create_database()
-    create_tables(app)
-    print("Таблиці даних вже створені або існують.")
-    populate_data()
-
-    # Виконання додаткових SQL-скриптів
-    execute_sql_scripts(['triggers.sql', 'functions.sql', 'stored_procedures.sql'])
-    print("SQL-скрипти виконано.")
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
